d1f608de4980a92022b2562e6d8f8390998b988f
Nethelbert <gilblasse@gmail.com>
2019-11-02 19:42:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
+require 'pry'
 # Your Code Here
+
+def map (array)
+  new_array = [] 
+  array.each {|value| new_array << (yield(value))} 
+  new_array
+end
+
+def reduce (array,starting_point=0)
+  total = starting_point
+  array.each {|value| total = yield(total,value)}
+  total
+end
